996a72526c7613781879d4d461e67e36dacede9b
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-18 13:58:12 +0530

    added database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express,{Request,Response} from  'express';
 import cors from 'cors';
 import 'dotenv/config';
-
-
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
