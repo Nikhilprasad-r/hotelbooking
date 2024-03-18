a955e99aa53ef95b4dc4cea423a8b401ceb682fa
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-18 13:20:20 +0530

    creating basic backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express,{Request,Response} from  'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/test', (req:Request, res:Response) => {
+  res.json({message: 'test'});
+});
+
+app.listen(7000,()=>{
+  console.log('Server running on port 7000');
+})
